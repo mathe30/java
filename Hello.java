acbbe6f3ba1b761074f05c1f183599a099f94c1d
mhsp <mohamed.hasep2822@gmail.com>
2018-02-24 16:58:36 +0300

    files added

---
+++ Hello.java
@@ -0,0 +1,28 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("* * * * *");
+		System.out.println(" * * * * *");
+		System.out.println("* * * * *");
+		System.out.println(" * * * * *");
+		System.out.println("* * * * *");
+		System.out.println();
+		
+		
+		System.out.println("* * * * *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("* * * * *");
+		System.out.println();
+		
+		System.out.println("* * * * *");
+		System.out.println(" *     *");
+		System.out.println("  *   *");
+		System.out.println("   * *");
+		System.out.println("    *");
+		System.out.println();
+
+		System.out.println("\n\n=========> Done .");
+	}
+}
