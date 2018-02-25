392228dcd59281e6e3e1dbd7a516f30db1fc4493
mhsp <mohamed.hasep2822@gmail.com>
2018-02-25 11:43:59 +0300

    Stuff re-arranged

---
+++ patterns.java
@@ -0,0 +1,26 @@
+class patterns {
+  public static void main(String[] args) {
+    System.out.println("* * * * *");
+    System.out.println(" * * * * *");
+    System.out.println("* * * * *");
+    System.out.println(" * * * * *");
+    System.out.println("* * * * *");
+    System.out.println();
+
+    System.out.println("* * * * *");
+    System.out.println("*       *");
+    System.out.println("*       *");
+    System.out.println("*       *");
+    System.out.println("* * * * *");
+    System.out.println();
+
+
+    System.out.println("* * * * *");
+    System.out.println(" *     *");
+    System.out.println("  *   *");
+    System.out.println("   * *");
+    System.out.println("    *");
+    System.out.println();
+    System.out.println("\n\n #===> done .");
+  }
+}
