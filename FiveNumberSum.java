c2e12b4882f7dbd9d2eeecefb8b97d1e26006ecd
mhsp <mohamed.hasep2822@gmail.com>
2018-02-26 12:53:14 +0300

    FiveNumbers & CircleComputation classes Added

---
+++ FiveNumberSum.java
@@ -0,0 +1,13 @@
+public class FiveNumberSum {
+	public static void main(String[] args){
+		int num1 = 11;
+		int num2 = 22;
+		int num3 = 33;
+		int num4 = 44;
+		int num5 = 55;
+		int sum;
+		sum = num1 + num2 + num3 + num4 + num5;
+		System.out.print("The sum is ");
+		System.out.println(sum);	
+	}
+}
