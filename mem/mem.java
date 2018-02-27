8108e468ff2318c26937b32a950084d2e9838e7c
mhsp <mohamed.hasep2822@gmail.com>
2018-02-27 14:34:55 +0300

    memory displayer added

---
+++ mem/mem.java
@@ -0,0 +1,28 @@
+public class mem{
+	public static void main(String[] args){
+		Runtime x = Runtime.getRuntime();
+		Runtime.Version v = Runtime.version();
+
+		long g = (1024);
+		
+		// g variable conatins the value to make the conversion 
+		//from the bytes that is returned from the Runtime memory functions
+		//by default .. so now we devide the values on the g value to convert to 
+		//kb -> mb -> gb 
+		//1024 * 1024 * 1024
+
+		long mm = x.maxMemory()/g;
+		long fm = x.freeMemory()/g;
+		long tm = x.totalMemory()/g;
+
+		System.out.println("Memory display v 1.0 ");
+		System.out.println();
+		System.out.println("Max memory is "+ mm);
+		System.out.println("Free memory is "+ fm);
+		System.out.println("Total memory is "+ tm);
+		System.out.println();
+		System.out.println("The Runtime version is "+ v);
+		System.out.println("=====> Done .");
+	}
+
+}
